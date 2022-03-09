7968ebf0d290bd320b11c62e891da920e90000af
Akila Sri M <akil3016@global.chainsys.com>
2022-03-09 12:56:42 +0530

    js

---
+++ src/advice.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+window.addEventListener('DOMContentLoaded', () => {
+  const getResource = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice')
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
+
+    return await res.json()
+  }
+
+  const title = document.querySelector('.title')
+  descr = document.querySelector('.descr')
+  btn = document.querySelector('.dice')
+
+  function setData (data) {
+    console.log(data)
+    title.textContent = 'advcice #' + data.id
+    descr.textContent = data.advice
+  }
+
+  btn.addEventListener('click', () => {
+    getResource().then(data => setData(data.slip))
+  })
+})
