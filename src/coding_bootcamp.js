f7cc50b3b1770c8d2ed44f04f53a589bffb9b0e4
Akila Sri M <akil3016@global.chainsys.com>
2022-03-09 18:25:04 +0530

    index

--- src/coding_bootcamp.js
+++ src/coding_bootcamp.js
@@ -1,42 +1,20 @@
-const prevBtn = document.querySelector('.prev')
-const nextBtn = document.querySelector('.next')
-const image = document.querySelector('.image')
-const feedback = document.querySelector('.feedback')
+const btnSwitch = document.querySelector('#content-switcher-1')
+const btnSwitch2 = document.querySelector('#content-switcher-2')
+const testinomialOne = document.querySelector('.testinomial-one')
+const testinomialSecond = document.querySelector('.testinomial-second')
 
-const users = [
-  {
-    name: 'Tanya Sinclair',
-    role: 'UX Engineer',
-    feedback: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
-    image: '/images/tanya.svg'
-  },
-  {
-    name: 'John Tarkpor',
-    role: 'Junior Front-end Developer',
-    feedback: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
-    image: '/images/john.svg'
+function fn (event) {
+  if (event.target.matches('img')) {
+    testinomialOne.classList.toggle('display-none')
+    testinomialSecond.classList.toggle('display-none')
   }
-]
-
-let slide = 0
-
-const navigation = () => {
-  // Check slide value
-  if (slide > users.length - 1) {
-    slide = 0
+  if (event.keyCode === 37 || event.keyCode === 39) {
+    testinomialOne.classList.toggle('display-none')
+    testinomialSecond.classList.toggle('display-none')
   }
-  image.innerHTML = `
-    <img src="${users[slide].image}" alt="">
-    `
-  feedback.innerHTML = `
-    <img src="/images/pattern-quotes.svg" alt="">
-    <p>${users[slide].feedback}</p>
-    <h4 class="name">${users[slide].name} <span class="role">${users[slide].role}</span></h4>
-    `
-  slide++
 }
 
-navigation()
+btnSwitch.addEventListener('click', fn)
+btnSwitch2.addEventListener('click', fn)
 
-prevBtn.addEventListener('click', navigation)
-nextBtn.addEventListener('click', navigation)
+window.addEventListener('keydown', fn)
